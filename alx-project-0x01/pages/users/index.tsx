035f493f689e5d8b02035a8497ad15013fadecd5
baby6969 <yafetzekarias23@fmail.com>
2025-11-23 14:40:38 +0300

    fix issues

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,12 +1,31 @@
 import React from "react";
+import UserCard from "@/components/common/UserCard";
+import { UserProps } from "@/interfaces";
 
-const UsersPage: React.FC = () => {
+interface UsersProps {
+  posts: UserProps[];
+}
+
+const Users = ({ posts }: UsersProps): JSX.Element => {
   return (
-    <main className="p-4">
+    <main className="p-4 flex flex-col items-center space-y-4">
       <h1 className="text-2xl font-bold mb-4">Users</h1>
-      <p>List of users will appear here</p>
+      {posts.map((user) => (
+        <UserCard key={user.id} user={user} />
+      ))}
     </main>
   );
 };
 
-export default UsersPage;
+export async function getStaticProps() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const posts: UserProps[] = await response.json();
+
+  return {
+    props: {
+      posts,
+    },
+  };
+}
+
+export default Users;
