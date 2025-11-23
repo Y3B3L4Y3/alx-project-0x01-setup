aa43d2df20fe72f8e3c3606582d2ecca7f8122b4
baby6969 <yafetzekarias23@fmail.com>
2025-11-23 14:36:12 +0300

    update

---
+++ alx-project-0x01/components/common/UserCard.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { UserProps } from "@/interfaces";
+
+interface UserCardProps {
+  user: UserProps;
+}
+
+const UserCard: React.FC<UserCardProps> = ({ user }) => {
+  return (
+    <div className="border rounded p-4 shadow-md w-full max-w-md bg-white">
+      <h2 className="text-xl font-bold">{user.name}</h2>
+      <p className="text-gray-600">@{user.username}</p>
+      <p>Email: {user.email}</p>
+      <p>Phone: {user.phone}</p>
+      <p>Website: {user.website}</p>
+      <p>
+        Address: {user.address.street}, {user.address.suite}, {user.address.city},{" "}
+        {user.address.zipcode}
+      </p>
+      <p>Company: {user.company.name}</p>
+    </div>
+  );
+};
+
+export default UserCard;
